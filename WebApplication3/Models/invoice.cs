1fb993880cacd51dd2fda545d1a4ba6f4776ee0c
sandifouad <44641093+sandifouad@users.noreply.github.com>
2018-11-07 16:39:58 +0200

    mdnidw
    mxncsakf

---
+++ WebApplication3/Models/invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class invoice
+    {
+        public string items;
+        public int service;
+        public int tax;
+        public int deliveryfee;
+        public int totalreciept;
+
+    }
+}
---
+++ WebApplication3/Models/user.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class user
+    {
+      public int id;  
+      public string username ;
+      public string firstname;
+      public string lastname;
+      public string email;
+      public string fullnumber;
+
+    }
+}
